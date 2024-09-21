90455ae890a849ec384419498e7bdc8a34880a32
정준우 <80523328+bviz6542@users.noreply.github.com>
2024-09-21 23:52:03 +0900

    JavaTaoApplication.java 업데이트

--- src/main/java/com/example/java_tao/JavaTaoApplication.java
+++ src/main/java/com/example/java_tao/JavaTaoApplication.java
@@ -4,7 +4,6 @@ import com.example.java_tao.leet.Solution;
 import com.example.java_tao.leet.Solution144;
 
 public class JavaTaoApplication {
-
     public static void main(String[] args) {
         Solution sol = new Solution144();
         sol.run();
